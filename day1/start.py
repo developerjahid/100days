28135b78493a5040114f64fade3008352ecbe37a
Developer Jahid <jhpabel@gmail.com>
2020-11-11 22:50:40 +0600

    day1 done

---
+++ day1/start.py
@@ -0,0 +1,12 @@
+# print something at console
+print("Hello 100 Days")
+
+# new line/ separate lines
+print("Hello 100 Days\nHello Day 1")
+
+# input name & get the name from console
+name = input('What is your name?')
+print("Your name is " + name)  # or
+print("Your name is {}".format(name))  # or
+print(f"Your name is {name}")  # or
+print("Your name is %s" % (name))
